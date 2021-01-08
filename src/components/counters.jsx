685b22baae050a2434484cf8d3f486167f264d8b
Eren Yetisgin <erenyetiskin@hotmail.com>
2021-01-08 21:41:54 +0300

    Reset button added

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,19 +11,37 @@ class Counters extends Component {
     ],
   };
 
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   handleDelete = (counterId) => {
     const counters = this.state.counters.filter((c) => c.id !== counterId);
     this.setState({ counters });
   };
 
   render() {
-    return this.state.counters.map((counter) => (
-      <Counter
-        key={counter.id}
-        onDelete={this.handleDelete}
-        counter={counter}
-      />
-    ));
+    return (
+      <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+        {this.state.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
+          />
+        ))}
+      </div>
+    );
   }
 }
 
